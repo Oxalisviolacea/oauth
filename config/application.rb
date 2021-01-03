0c4e200bef661a098f09593c48c964f86ed5c503
oxalisviolacea <hannadavis09@gmail.com>
2021-01-03 13:16:54 -0700

    Add omni auth middleware

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,10 @@ module Oauth
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.use OmniAuth::Builder do
+      provider :developer if Rails.env.development?
+      provider :github, 'c67c7412d30691d23375', '7cd3f397f7139eb817625a8c9e783c47af303bdb'
+    end
   end
 end
