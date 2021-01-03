790175799404ae0d18994158d36ec2d1484e5f53
oxalisviolacea <hannadavis09@gmail.com>
2021-01-03 13:17:27 -0700

    Add routes to home, omniauth, and repos page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: "pages#home"
+  get '/private', to: "pages#private"
+
+  get 'auth/developer', :as => 'developer_auth'
+  get 'auth/github', :as => 'github_auth'
+  match 'auth/:provider/callback' => "session#create", :via => [:get, :post]
+  get 'session/destroy', :as => 'logout'
+
 end
